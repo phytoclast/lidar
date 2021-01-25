cf79b98a3e575cdfe876fa623d0419ddac6c05ea
phytoclast2 <76961867+phytoclast2@users.noreply.github.com>
2021-01-25 16:33:57 -0500

    Phase 3
    Gave up on reprojecting LAS files. After fixing an error in new CRS for reprojecting, it stopped working. So I focused on reprojecting the rasters. Experimenting with finer resolutions to resample ultimately to 5 m.

--- Lidar.R
+++ Lidar.R
@@ -1,4 +1,3 @@
-#remotes::install_github("Jean-Romain/lidR", ref = "devel")
 library(lidR)
 library(rgdal)
 library(rlas)
@@ -20,37 +19,56 @@ file.original <- fl[1]
 Las <- readLAS(paste0(path,"/",file.original), filter="-drop_class 7 18")
 crs.old <- as.character(Las@proj4string)
 crs.new <- str_replace(crs.old, '\\+units\\=ft','\\+units\\=m')
-crs.new <- str_replace(crs.old, '\\+vunits\\=ft','\\+vunits\\=m')
+crs.new <- str_replace(crs.new, '\\+vunits\\=ft','\\+vunits\\=m')
 isvfeet <- grepl('+vunits=ft', crs.old)
 ishmeter <- grepl('+units=m', crs.old)
+if(F){ #disabled, because it doesn't work
 for (i in 1:length(fl)){
   file.original <- fl[i]
   Las <- readLAS(paste0(path,"/",file.original), filter="-drop_class 7 18")
   if(ishmeter){
     Las2 <- Las
     }else{
-  Las2 <- lastransform(Las, CRS(crs.new))}
+  Las2 <- spTransform(Las, CRS(crs.new))}
   if(isvfeet){
   Las2$Z <- Las2$Z *.3048}
+  #fix header ....
+  #header.old <- wkt(Las)
+  #header.new <- str_replace_all(header.old, 'foot','meter')
+  #header.new <- str_replace_all(header.new, '\\(ft\\)','\\(m\\)')
+  #header.new <- str_replace_all(header.new, "0.3048", "1")
+  #wkt(Las2) <- header.new
+  wkt(Las2) <- wkt(crs(Las2))
   file.new <- paste0(stringr::str_split_fixed(file.original,'\\.',2)[1],'.las')
   writeLAS(Las2, paste0(path.new,'/',file.new))
-}
+}}
 #create a catalog of pre-metricated lidar tiles ----
 #
-las.collection <- readLAScatalog(path.new)
+#las.collection <- readLAScatalog(path.new)
+las.collection <- readLAScatalog(path)
+res <- 5
+res.new <- res
+if(!ishmeter){res.new <- res/0.3048}
 
-#opt_filter(las.collection) = '2'
+ground <- grid_terrain(las.collection, res = res.new/3, algorithm = tin())
+if(isvfeet){ground <- ground * 0.3048}
+ground[ground > 10000 | ground < -10000] <- NA
+if(!ishmeter){ground <- projectRaster(ground, crs = CRS(crs.new), method = 'bilinear', res = res/3)}
+writeRaster(ground, paste0(path.new,'/','ground.tif'), overwrite=T)
 
-dtm_tin <- grid_terrain(las.collection, res = 5, algorithm = tin())
-writeRaster(dtm_tin, paste0(path.new,'/','ground.tif'), overwrite=T)
-dsm <- grid_canopy(las.collection, res = 5, algorithm = dsmtin())
-writeRaster(dsm, paste0(path.new,'/','surface.tif'), overwrite=T)
-plot(dsm)
-canopy <- dsm - dtm_tin
-canopy[canopy > 115] <- NA
+surface <- grid_canopy(las.collection, res = res.new/3, algorithm = dsmtin())
+if(isvfeet){surface <- surface * 0.3048}
+surface[surface > 10000 | surface < -10000] <- NA
+if(!ishmeter){surface <- projectRaster(surface, crs = CRS(crs.new), method = 'bilinear', res = res/3)}
+writeRaster(surface, paste0(path.new,'/','surface.tif'), overwrite=T)
+plot(surface)
+
+canopy <- surface - ground
+canopy[canopy > 150] <- NA
+#canopy.max <- focal(canopy, w = focalWeight(canopy, d=5, type='circle'), fun = max, na.rm=T)
+canopy.max <- focal(canopy, w=matrix(1,3,3), fun = max, na.rm=T)
 writeRaster(canopy, paste0(path.new,'/','canopy.tif'), overwrite=T)
-plot(canopy)
+writeRaster(canopy.max, paste0(path.new,'/','canopy.max.tif'), overwrite=T)
 plot(canopy, breaks = c(0,5,10,20,30,40,50), col=c('white', 'lightgreen', 'darkgreen', 'yellow', 'orange', 'red', 'purple'))
 
-
-
+plot(ground)
