a6f5b17736af1bb9e232d890c774de0d59dfe6c0
phytoclast2 <76961867+phytoclast2@users.noreply.github.com>
2021-03-11 10:19:31 -0500

    Update ternarygraphs.R

--- ternarygraphs.R
+++ ternarygraphs.R
@@ -173,19 +173,3 @@ par(mar = c(1,1,1,1))
 cummulative
 dev.off()
 
-#crowns
-library(plyr)
-library(Hmisc)
-crown.dist <- read.csv('output/crowndist.csv')
-
-percentiles.crowns <- ddply(crown.dist, .(site), summarise,
-                            width50 = round(wtd.quantile(width,weights=area, 0.5),2),
-                            canopy50 = round(wtd.quantile(ht50,weights=area, 0.5),2),
-                            crown05 = round(wtd.quantile(htmax,weights=area, 0.05),2),
-                            crown25 = round(wtd.quantile(htmax,weights=area, 0.25),2),
-                            crown50 = round(wtd.quantile(htmax,weights=area, 0.50),2),
-                            crown75 = round(wtd.quantile(htmax,weights=area, 0.75),2),
-                            crown95 = round(wtd.quantile(htmax,weights=area, 0.95),2),
-                            crownmax = round(max(htmax),2)
-)
-write.csv(percentiles.crowns,'output/crownsum.csv', row.names = F)
