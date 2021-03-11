8c5c8bad8de2bf8cb4cb0f86658167d2724012d3
phytoclast2 <76961867+phytoclast2@users.noreply.github.com>
2021-03-11 10:19:33 -0500

    Update Crown_summary.R

--- Crown_summary.R
+++ Crown_summary.R
@@ -1,58 +1,17 @@
-library(lidR)
 library(plyr)
-library(stringr)
-library(terra)
-library(sf)
-library(fasterize)
-library(ggplot2)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-stands <- st_read('data/stands.shp')
-crowndist <- 0
+library(Hmisc)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))#crowns
 
-folderlist <- unique((stands$folder))
+crown.dist <- read.csv('output/crowndist.csv')
 
-for (i in 1:length(folderlist)){ #i=20
-
-path <- paste0('output/',folderlist[i])
-
-canopy <- rast(paste0(path,'/','canopy.tif'))
-crowns <-  rast(paste0(path,'/','crowns.tif'))
-veg <- subset(stands, folder %in% folderlist[i])
-veg$id <- as.numeric(rownames(veg))
-veg.t <- st_drop_geometry(veg)
-
-veg <- st_transform(veg, crs = crs(canopy))
-veg.r <- fasterize(veg, raster(canopy), field = 'id')
-writeRaster(veg.r, 'output/veg.r.tif', overwrite=TRUE)
-veg.r <- rast('output/veg.r.tif')
-#plot(veg.r)
-##frequency of gaps and emergents ----
-
-  vht <- c(veg.r, canopy, crowns)
-  vht <- as.data.frame(vht, xy=T)
-  colnames(vht) <- c('x', 'y', 'id', 'ht', 'crown')
-
-vht <- subset(vht, !ht >116)
-vht <- merge(vht, veg.t, by='id')
-
-#calculate percentage above 5 meters ----
-
-
-#determine and remove outliers -----
-#get median and 5-95 quantiles ----
-percentiles.trees <- ddply(vht, .(site, crown), summarise,
-                           area = length(ht), 
-                           ht50 = quantile(ht, 0.5),
-                           htmax = max(ht)
+percentiles.crowns <- ddply(crown.dist, .(site), summarise,
+                            width50 = round(wtd.quantile(width,weights=area, 0.5),2),
+                            canopy50 = round(wtd.quantile(ht50,weights=area, 0.5),2),
+                            crown05 = round(wtd.quantile(htmax,weights=area, 0.05),2),
+                            crown25 = round(wtd.quantile(htmax,weights=area, 0.25),2),
+                            crown50 = round(wtd.quantile(htmax,weights=area, 0.50),2),
+                            crown75 = round(wtd.quantile(htmax,weights=area, 0.75),2),
+                            crown95 = round(wtd.quantile(htmax,weights=area, 0.95),2),
+                            crownmax = round(max(htmax),2)
 )
-percentiles.trees$width <-  (percentiles.trees$area/3.141592)^0.5*2
-
-if(length(crowndist)>1){
-  crowndist <- rbind(crowndist, percentiles.trees)
-}else{crowndist <- percentiles.trees}
-
-
-}
-  write.csv(crowndist, 'output/crowndist.csv', row.names = F)
-  
-  
+write.csv(percentiles.crowns,'output/crownsum.csv', row.names = F)
