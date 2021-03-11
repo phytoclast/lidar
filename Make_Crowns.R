c52d477deec25d78ce80d28cf1bcb4ecadf3fe34
phytoclast2 <76961867+phytoclast2@users.noreply.github.com>
2021-03-11 11:49:41 -0500

    Update Make_Crowns.R

--- Make_Crowns.R
+++ Make_Crowns.R
@@ -1,58 +1,49 @@
-library(lidR)
-library(plyr)
-library(stringr)
-library(terra)
-library(sf)
-library(fasterize)
-library(ggplot2)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-stands <- st_read('data/stands.shp')
-crowndist <- 0
-
-folderlist <- unique((stands$folder))
-
-for (i in 1:length(folderlist)){ #i=20
-
-path <- paste0('output/',folderlist[i])
-
-canopy <- rast(paste0(path,'/','canopy.tif'))
-crowns <-  rast(paste0(path,'/','crowns.tif'))
-veg <- subset(stands, folder %in% folderlist[i])
-veg$id <- as.numeric(rownames(veg))
-veg.t <- st_drop_geometry(veg)
-
-veg <- st_transform(veg, crs = crs(canopy))
-veg.r <- fasterize(veg, raster(canopy), field = 'id')
-writeRaster(veg.r, 'output/veg.r.tif', overwrite=TRUE)
-veg.r <- rast('output/veg.r.tif')
-#plot(veg.r)
-##frequency of gaps and emergents ----
-
-  vht <- c(veg.r, canopy, crowns)
-  vht <- as.data.frame(vht, xy=T)
-  colnames(vht) <- c('x', 'y', 'id', 'ht', 'crown')
-
-vht <- subset(vht, !ht >116)
-vht <- merge(vht, veg.t, by='id')
-
-#calculate percentage above 5 meters ----
-
-
-#determine and remove outliers -----
-#get median and 5-95 quantiles ----
-percentiles.trees <- ddply(vht, .(site, crown), summarise,
-                           area = length(ht), 
-                           ht50 = quantile(ht, 0.5),
-                           htmax = max(ht)
-)
-percentiles.trees$width <-  (percentiles.trees$area/3.141592)^0.5*2
-
-if(length(crowndist)>1){
-  crowndist <- rbind(crowndist, percentiles.trees)
-}else{crowndist <- percentiles.trees}
-
-
-}
-  write.csv(crowndist, 'output/crowndist.csv', row.names = F)
+  library(lidR)
+  library(plyr)
+  library(stringr)
+  library(terra)
+  library(sf)
+  library(fasterize)
+  library(ggplot2)
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  stands <- st_read('data/stands.shp')
   
+  folderlist <- unique((stands$folder))
+  #function to determine crown search width
+  f <- function(x) {
+    y <- x^0.5/30^0.5*15
+    y[y < 5] <- 5
+    y[y > 25] <- 25
+    return(y)
+  }
+  
+  heights <- seq(-5,100,0.5)
+  ws <- f(heights)
+  plot(heights, ws, type = "l",  ylim = c(0,30))
+  
+ 
+  for (i in 1:length(folderlist)){
+  
+  path <- paste0('output/',folderlist[i])
+  if(!file.exists(paste0(path,'/','crowns.tif'))){
+  canopy <- raster(paste0(path,'/','canopy.tif'))
+  
+  #find tops of trees
+  ttops <- find_trees(canopy, lmf(f, hmin=5, shape='circular'))
+  
+  ttops2 <-  st_as_sf(ttops)
+  st_write(ttops2,paste0(path,'/','ttps.shp'), driver="ESRI Shapefile", overwrite =TRUE, append = FALSE)
+  #delineate crown boundaries
+  canopy.trees <- dalponte2016(canopy, ttops, th_tree = 5, th_seed = 0.45, th_cr = 0.55, max_cr = 25, ID = "treeID")
+  crowns <- canopy.trees()
+  plot(crowns, col = pastel.colors(1000))
+  writeRaster(crowns, filename= paste0(path,'/','crowns.tif'),
+              overwrite=TRUE, wopt=list(gdal=c("COMPRESS=LZW")))
+  v <- terra::as.polygons(rast(crowns))
+  crowns.sf <- st_as_sf(as.data.frame(v, geom=TRUE), wkt="geometry", crs=crs(canopy))
+  
+  crowns.sf <- merge(crowns.sf, st_drop_geometry(ttops2), by.x='layer', by.y='treeID')
+  st_write(crowns.sf,paste0(path,'/','crowns.shp'), driver="ESRI Shapefile", overwrite =TRUE, append = FALSE)
+  }
+  }
   
