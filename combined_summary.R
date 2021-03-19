1eb03f3c1f1d3aa33bf817c3b20b35b6000bd4df
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2021-03-18 22:13:02 -0400

    implement key

--- combined_summary.R
+++ combined_summary.R
@@ -15,3 +15,24 @@ combsum$tree60 <- round(combsum$pretree60*combsum$strattree/100,2)
 combsum <- subset(combsum, select = -c(pretree05, pretree15, pretree30, pretree45, pretree60))
 
 write.csv(combsum,'output/combsum.csv', row.names = F)
+
+v <- read.csv('output/combsum.csv')
+
+v$base <- ifelse((v$strattree >= 10 & v$crown50 >=5), 
+                ifelse(v$strattree >= 75, 'forest','woodland'),
+                ifelse(v$strat02 >= 10, 'shrubland','open'))
+
+v$mod1 <- ifelse(v$base %in%c('woodland','forest'), 
+                 ifelse(v$crown50 < 15, 'low',
+                        ifelse(v$crown50 < 30, 'medium',
+                               ifelse(v$c100m < 45, 'high',
+                                      ifelse(v$crown50 < 45|v$c100m < 60, 'tall','giant')))),'')
+
+v$mod2 <- ifelse(v$base %in%c('woodland'), 
+                 ifelse(v$strat02 >= 25, 'shrubby','open'),
+                 ifelse(v$base %in%c('shrubland'), 
+                        ifelse(v$strat02 >= 25, 'dense','open'),''))
+
+v$veg <-stringr::str_squish(paste(v$mod2, v$mod1, v$base))
+             
+write.csv(v,'output/keyveg.csv', row.names = F)
